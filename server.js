9906021fef43f6481ead0e10685f01ca9be0d297
Priyanshu Kumar Singh <pks9918705@gmail.com>
2024-07-16 13:49:38 +0530

    Updated code

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ io.on('connection', (socket) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
